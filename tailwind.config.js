37d380d82320fbee9d6a40f7b1816a208ca54bc0
novita19931993 <148832692+novita19931993@users.noreply.github.com>
2025-03-06 19:44:06 -0300

    arreglando tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de que esta línea esté presente
+    "./index.html",  // Si tu index.html está en la raíz
+    "./info-kuuwu/**/*.{js,ts,jsx,tsx,html}", // Si el código está dentro de infokuuwu
+    "./src/**/*.{js,ts,jsx,tsx,html}", // Para archivos dentro de src
   ],
   theme: {
     extend: {},
